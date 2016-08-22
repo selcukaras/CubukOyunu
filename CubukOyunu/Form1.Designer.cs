1fb64e97cbfdc1534f580e84d20c9f8c03566636
selcukaras <selcukalan.net@gmail.com>
2016-08-22 15:02:33 +0300

    cubuk

---
+++ CubukOyunu/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace CubukOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.boxCubuk = new System.Windows.Forms.PictureBox();
+            this.kutu1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.boxCubuk)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kutu1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // boxCubuk
+            // 
+            this.boxCubuk.BackColor = System.Drawing.Color.Red;
+            this.boxCubuk.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.boxCubuk.Image = global::CubukOyunu.Properties.Resources.cubuk;
+            this.boxCubuk.Location = new System.Drawing.Point(455, 538);
+            this.boxCubuk.Margin = new System.Windows.Forms.Padding(4);
+            this.boxCubuk.Name = "boxCubuk";
+            this.boxCubuk.Size = new System.Drawing.Size(210, 40);
+            this.boxCubuk.TabIndex = 0;
+            this.boxCubuk.TabStop = false;
+            // 
+            // kutu1
+            // 
+            this.kutu1.BackColor = System.Drawing.Color.Yellow;
+            this.kutu1.InitialImage = null;
+            this.kutu1.Location = new System.Drawing.Point(19, 50);
+            this.kutu1.Margin = new System.Windows.Forms.Padding(4);
+            this.kutu1.Name = "kutu1";
+            this.kutu1.Size = new System.Drawing.Size(101, 100);
+            this.kutu1.TabIndex = 1;
+            this.kutu1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.pictureBox1.Location = new System.Drawing.Point(128, 50);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(101, 100);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.pictureBox2.Location = new System.Drawing.Point(237, 50);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(101, 100);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Fuchsia;
+            this.pictureBox3.Location = new System.Drawing.Point(346, 50);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(101, 100);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Aqua;
+            this.pictureBox4.Location = new System.Drawing.Point(455, 50);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(101, 100);
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.pictureBox5.Location = new System.Drawing.Point(564, 50);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(101, 100);
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.pictureBox6.Location = new System.Drawing.Point(673, 50);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(101, 100);
+            this.pictureBox6.TabIndex = 7;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.Green;
+            this.pictureBox7.Location = new System.Drawing.Point(782, 50);
+            this.pictureBox7.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(101, 100);
+            this.pictureBox7.TabIndex = 8;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBox8.Location = new System.Drawing.Point(891, 50);
+            this.pictureBox8.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(101, 100);
+            this.pictureBox8.TabIndex = 9;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.Color.Purple;
+            this.pictureBox9.Location = new System.Drawing.Point(1000, 50);
+            this.pictureBox9.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(101, 100);
+            this.pictureBox9.TabIndex = 10;
+            this.pictureBox9.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(807, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 24);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Çarpma: 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 22.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(445, 302);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(275, 55);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Kaybettiniz:((";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(417, 11);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 24);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Puan: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.BackgroundImage = global::CubukOyunu.Properties.Resources.indir;
+            this.ClientSize = new System.Drawing.Size(1124, 652);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.kutu1);
+            this.Controls.Add(this.boxCubuk);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Çubuk";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.boxCubuk)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kutu1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox boxCubuk;
+        private System.Windows.Forms.PictureBox kutu1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CubukOyunu/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CubukOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int LR, tick, carpma, puan;
+        private int[] kutuHareket;
+        private int[] kutuCarpan;
+        private PictureBox[] kutular;
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                LR = 2;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                LR = 1;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (LR == 1)
+            {
+                CubukSola();
+            }
+            else if (LR == 2)
+            {
+                CubukSaga();
+            }
+            if (tick == 40)
+            {
+                KutuGonder();
+                bool gecti = KutularGectimi();
+                if (gecti)
+                {
+                    label2.Text = "Kazandınız:))";
+                    label2.Visible = true;
+                    timer1.Enabled = false;
+                }
+                tick = 0;
+            }
+            int i = 0;
+            while (i < 10)
+            {
+                if (kutuHareket[i] == 1)
+                {
+                    kutular[i].Top += 10;
+                }
+                i++;
+            }
+            Carpma();
+            tick++;
+        }
+
+        private bool Carpma()
+        {
+            int i = 0;
+            Rectangle cubuk = new Rectangle(boxCubuk.Location.X, boxCubuk.Location.Y, boxCubuk.Width, boxCubuk.Height);
+            
+            while (i < 10)
+            {
+                Rectangle kutu = new Rectangle(kutular[i].Location.X, kutular[i].Location.Y, kutular[i].Width, kutular[i].Height);
+                if (kutuCarpan[i]==0 && !Rectangle.Intersect(cubuk, kutu).IsEmpty)
+                {
+                    kutuCarpan[i] = 1; carpma++;
+                    label1.Text = "Çarpma: " + carpma.ToString();
+                    if (carpma==4)
+                    {
+                        label2.Visible = true;
+                        timer1.Enabled = false;
+                    }
+                }
+                if (kutuCarpan[i]==0 && kutu.Top>cubuk.Bottom)
+                {
+                    puan += 10;
+                    label3.Text = "Puan: " + puan.ToString();
+                    kutuCarpan[i] = 2;
+                }
+                //int kl = kutular[i].Left, kr = kutular[i].Right, ku = kutular[i].Top, kd = kutular[i].Bottom;
+                //int cl = boxCubuk.Left, cr = boxCubuk.Right, cu = boxCubuk.Top, cd = boxCubuk.Bottom;
+                //bool kutu_sol_dahil = kl < cr && kl > cl, 
+                //    kutu_sag_dahil = kr < cr && kr > cl,
+                //    cubuk_ust_dahil = kd > cu && ku < cu,
+                //    cubuk_alt_dahil = kd > cd && ku < cu;
+                //if (kutuCarpan[i] == 0 && (kutu_sag_dahil || kutu_sol_dahil) && (cubuk_ust_dahil || cubuk_alt_dahil))
+                //{
+                //    kutuCarpan[i] = 1; carpma++;
+                //}
+                i++;
+            }
+            return false;
+        }
+
+        private bool KutularGectimi()
+        {
+            bool ret=true;
+            foreach (var kutu in kutular)
+            {
+                if (kutu.Top<boxCubuk.Bottom)
+                {
+                    ret = false;
+                    break;
+                }
+            }
+            return ret;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            LR = 0;
+        }
+
+        private void CubukSola()
+        {
+            if (boxCubuk.Left > 10) { boxCubuk.Left -= 10; }
+        }
+
+        private void CubukSaga()
+        {
+            if (boxCubuk.Left + boxCubuk.Width < this.Width - 20) { boxCubuk.Left += 10; }
+        }
+
+        private void KutuGonder()
+        {
+            Random rnd = new Random();
+            int _r,_j=0;
+            while (true)
+            {
+                _r = rnd.Next(10);
+                if (kutuHareket[_r] == 0)
+                {
+                    kutuHareket[_r] = 1; break;
+                }
+                _j++;
+                if (_j==100)
+                {
+                    break;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            puan = 0;
+            kutuHareket = new int[10];
+            kutular = new PictureBox[10];
+            kutuCarpan = new int[10];
+            kutular[0] = kutu1; kutular[1] = pictureBox1;
+            kutular[2] = pictureBox2; kutular[3] = pictureBox3;
+            kutular[4] = pictureBox4; kutular[5] = pictureBox5;
+            kutular[6] = pictureBox6; kutular[7] = pictureBox7;
+            kutular[8] = pictureBox8; kutular[9] = pictureBox9;
+            int i = 0; while (i < 10) { kutuHareket[i] = 0; i++; }
+        }
+
+
+    }
+}
